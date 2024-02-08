486ee73339a006d24b8b8d0702bbfdc385528748
TheZero0-ctrl <ankitpariyar486@gmail.com>
2024-02-08 14:56:02 +0545

    start to update datatable to 1.13.7

--- app/assets/javascripts/dataTables/jquery.dataTables.js
+++ app/assets/javascripts/dataTables/jquery.dataTables.js
@@ -1,15 +1,15 @@
-/*! DataTables 1.10.10
- * ©2008-2015 SpryMedia Ltd - datatables.net/license
+/*! DataTables 1.13.7
+ * ©2018 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.10
+ * @version     1.13.7
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
- * @copyright   Copyright 2008-2015 SpryMedia Ltd.
+ * @copyright   Copyright 2018 SpryMedia Ltd - datatables.net/license
  *
  * This source file is free software, available under the following license:
  *   MIT license - http://datatables.net/license
